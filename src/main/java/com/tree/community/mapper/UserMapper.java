2c0ecdee17573a66d625dbce1ca34149152c9572
shaozihang <shaokoko8@126.com>
2020-01-26 22:12:43 +0800

    add flyway migration support

--- src/main/java/com/tree/community/mapper/UserMapper.java
+++ src/main/java/com/tree/community/mapper/UserMapper.java
@@ -3,9 +3,16 @@ package com.tree.community.mapper;
 import com.tree.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @Mapper
 public interface UserMapper {
+
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token=#{token}")
+    User findByToken(String token);
 }
