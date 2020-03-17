564e04743305357fb38bf1a9f2a9b7e05018c67e
shaozihang <shaokoko8@126.com>
2020-03-17 12:04:13 +0800

    头像修改

--- src/main/java/com/tree/community/controller/FileController.java
+++ src/main/java/com/tree/community/controller/FileController.java
@@ -1,22 +1,32 @@
 package com.tree.community.controller;
 
 import com.tree.community.dto.FileDTO;
+import com.tree.community.model.User;
+import com.tree.community.provider.AliyunProvider;
 import com.tree.community.provider.UCloudProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class FileController {
 
     @Autowired
     private UCloudProvider uCloudProvider;
+
+    @Autowired
+    private AliyunProvider aliyunProvider;
 
     @ResponseBody
     @RequestMapping("/file/upload")
@@ -38,5 +48,31 @@ public class FileController {
         fileDTO.setSuccess(1);
         fileDTO.setUrl("/images/wechat.png");
         return fileDTO;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/file/uploadAvatar/")
+    public Map<String,Object> uploadAvatar(@RequestParam("file") MultipartFile file,HttpServletRequest request){
+
+        User user = (User) request.getSession().getAttribute("user");
+        Map map = new HashMap();
+        try {
+            if(file == null){
+                map.put("code",0);
+            }else {
+                String result = aliyunProvider.uploadAvatar(file, user);
+                if(result.equals("success")){
+                    map.put("code",200);
+                }else {
+                    map.put("code",0);
+                }
+            }
+
+        } catch (Exception e) {
+            map.put("code",0);
+            e.printStackTrace();
+        }
+
+        return map;
     }
 }
---
+++ src/main/resources/db/migration/V29__Update_avatar_url_length_to_user_table.sql
@@ -0,0 +1,2 @@
+alter table `tree`.`user`
+change column `avatar_url` `avatar_url` varchar(200) character set 'utf8mb4' collate 'utf8mb4_bin' null default null ;
